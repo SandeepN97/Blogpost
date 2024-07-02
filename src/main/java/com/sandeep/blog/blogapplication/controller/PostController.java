320a7409d0977712e1134ff8c5cd2e0223a48fa5
SurajMaharjan007 <surajmaharjan222@gmail.com>
2024-07-02 10:11:26 -0500

    Worked on getPostById on PostController and PostService

--- src/main/java/com/sandeep/blog/blogapplication/controller/PostController.java
+++ src/main/java/com/sandeep/blog/blogapplication/controller/PostController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,8 @@ public class PostController {
         return postService.getAllPost();
     }
 
-
+    @GetMapping("/getPost/{id}")
+    public  ResponseEntity<Post> getPostById (@PathVariable long id){
+        return postService.getPostById(id);
+    }
 }
