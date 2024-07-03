0777bb811ab1bebc42aedc1336705f64de20dac3
Sandeep Nyoupane <65792729+SandeepN97@users.noreply.github.com>
2024-07-03 16:27:46 -0500

    Add CRUD operations to Post controller and service
    - Implement GET, POST, PUT, DELETE operations
    - Update PostController with methods for each operation
    - Update PostService to handle business logic
    - Define Post model for data structure

--- src/main/java/com/sandeep/blog/blogapplication/controller/PostController.java
+++ src/main/java/com/sandeep/blog/blogapplication/controller/PostController.java
@@ -5,9 +5,13 @@ import com.sandeep.blog.blogapplication.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 
+<<<<<<< Updated upstream
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+=======
+import org.springframework.web.bind.annotation.*;
+>>>>>>> Stashed changes
 
 import java.util.List;
 
@@ -22,5 +26,37 @@ public class PostController {
         return postService.getAllPost();
     }
 
+<<<<<<< Updated upstream
 
+=======
+    @GetMapping("/getPost/{id}")
+    public  ResponseEntity<Post> getPostById (@PathVariable long id){
+        return postService.getPostById(id);
+    }
+
+    @GetMapping("/getPostByTitle/{title}")
+    public ResponseEntity<Post> getPostByTitle(@PathVariable String title){
+        return postService.getPostByTitle(title);
+    }
+
+    @GetMapping("/getPostByUser/{userId}")
+    public ResponseEntity<List<Post>> getPostByUser(@PathVariable long userId){
+        return postService.getPostByUser(userId);
+    }
+
+    @PostMapping("/addPost")
+    public ResponseEntity<Post> addPost(@RequestBody Post post){
+        return postService.addPost(post);
+    }
+
+    @PutMapping("/updatePost/{id}")
+    public ResponseEntity<Post> updatePost(@PathVariable long id, @RequestBody Post post){
+        return postService.updatePost(id, post);
+    }
+
+    @DeleteMapping("/deletePost/{id}")
+    public ResponseEntity<Post> deletePost(@PathVariable long id){
+        return postService.deletePost(id);
+    }
+>>>>>>> Stashed changes
 }
--- src/main/java/com/sandeep/blog/blogapplication/service/PostService.java
+++ src/main/java/com/sandeep/blog/blogapplication/service/PostService.java
@@ -22,4 +22,90 @@ public class PostService {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+<<<<<<< Updated upstream
+=======
+
+    public ResponseEntity<Post> getPostById(long id) {
+        try {
+            Optional<Post> optionalPost = postRepository.findById(id);
+            if(optionalPost.isPresent()){
+                Post postToBeFound = optionalPost.get();
+                return  new ResponseEntity<>(postToBeFound, HttpStatus.OK);
+            }else{
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        }
+        catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public ResponseEntity<Post> getPostByTitle(String title) {
+        try {
+            Optional<Post> optionalPost = postRepository.findByTitle(title);
+            if(optionalPost.isPresent()){
+                Post postToBeFound = optionalPost.get();
+                return  new ResponseEntity<>(postToBeFound, HttpStatus.OK);
+            }else{
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        }
+        catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public ResponseEntity<List<Post>> getPostByUser(long userId) {
+        try {
+            List<Post> post= postRepository.findByUserId(userId);
+            return new ResponseEntity<>(post,HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public ResponseEntity<Post> addPost(Post post) {
+        try {
+            Post postToBeAdded = postRepository.save(post);
+            return new ResponseEntity<>(postToBeAdded,HttpStatus.CREATED);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public ResponseEntity<Post> updatePost(long id, Post post) {
+        try {
+            Optional<Post> optionalPost = postRepository.findById(id);
+            if(optionalPost.isPresent()){
+                Post postToBeUpdated = optionalPost.get();
+                postToBeUpdated.setTitle(post.getTitle());
+                postToBeUpdated.setContent(post.getContent());
+                postToBeUpdated.setTags(post.getTags());
+                postToBeUpdated.setUser(post.getUser());
+                postToBeUpdated.setComments(post.getComments());
+                postToBeUpdated = postRepository.save(postToBeUpdated);
+                return new ResponseEntity<>(postToBeUpdated, HttpStatus.OK);
+            }else{
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public ResponseEntity<Post> deletePost(long id) {
+        try {
+            Optional<Post> optionalPost = postRepository.findById(id);
+            if(optionalPost.isPresent()){
+                Post postToBeDeleted = optionalPost.get();
+                postRepository.delete(postToBeDeleted);
+                return new ResponseEntity<>(postToBeDeleted, HttpStatus.OK);
+            }else{
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+>>>>>>> Stashed changes
 }
